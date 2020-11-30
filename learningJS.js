202e332c2a802527f51c4ba9b4fcea4d10a12f86
Krzysztof Baranski <krzysztofbaranski@KrzysztofsMBP2.fritz.box>
2020-11-30 12:46:54 +0100

    Values, Variables

---
+++ learningJS.js
@@ -0,0 +1,32 @@
+//  fundamentals 1
+
+//  linking JavaScript File
+let js = "amazing";
+console.log(40 + 25 + 10 - 5);
+
+// Values and Variables
+console.log("Kris");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "KB";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+
