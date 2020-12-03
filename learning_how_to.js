bee4143577eebd48c5d1200bd4e1b0ef8b01228b
Kris Baranski <krisbaranski@gmail.com>
2020-12-03 18:16:27 +0100

    Basic Operations

--- learning_how_to.js
+++ learning_how_to.js
@@ -1,6 +1,79 @@
-//  fundamentals 1
-
-//  linking JavaScript File
+//  Fundamentals 1
+/*
+// linking JavaScript File
 let js = "amazing";
 console.log(40 + 25 + 10 - 5);
 
+// Values and Variables
+console.log("Kris");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "KB";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+
+// // Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+
+// Basic Operators
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
