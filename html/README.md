5d399c08ee73b970b08791a9aab95eb712fb0b19
Krzysztof Baranski <krisbaranski@gmail.com>
2021-09-16 11:25:06 +0200

    feat: Start new project with React JS

---
+++ html/README.md
@@ -0,0 +1,8 @@
+# HTML and CSS basics
+
+![HTML Structure](/pics/01.png)
+![Semantic CSS](/pics/02.png)
+![CSS Styles](/pics/03.png)
+![Visual Rules](/pics/04.png)
+![Box Model](/pics/05.png)
+![New Box](/pics/06.png)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className='container'>
+      <header className='header'>
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
