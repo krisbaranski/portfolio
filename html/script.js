bc3b9405da9469841cfc0a03d633a9643ff5b780
Krzysztof Baranski <krisbaranski@gmail.com>
2021-09-17 22:17:26 +0200

    feat: Restructure markup, add variables, update css: header, hero, fonts

--- html/script.js
+++ html/script.js
@@ -2,7 +2,18 @@
 
 const hero = document.getElementById('hero');
 const colors = ['turquoise', '#2ecc71', '#f6e58d', '#eb4d4b'];
-const squares = 8000;
+const squares = 3500;
+
+let heroTitle = document.querySelector('.hero_title');
+let heroMessage = document.querySelector('.hero_message');
+
+heroTitle.innerHTML = 'Hello World';
+heroMessage.innerHTML =
+  `Welcome to my portfolio. My name is Kris. ` +
+  '<br/>' +
+  `I'm frontend engineer passioned with coding,  discovering new technologies and using them beyond borders.` +
+  '<br/>' +
+  `If you like my work... leave a comment below or contact me. I'm open for new projects`;
 
 for (let i = 0; i < squares; i++) {
   const square = document.createElement('div');
@@ -20,11 +31,15 @@ function setColor(element) {
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
+//
+// If we want color square to disapeare after particular time, use this function
+//
 // function removeColor(element) {
 //   element.style.background = '#1d1d1d';
 //   element.style.boxShadow = '0 0 2px #000';
 //   element.style.transition = '25s ease';
 // }
+//
 
 function getRandomColor() {
   return colors[Math.floor(Math.random() * colors.length)];
