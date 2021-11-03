641d3f27eda21c1b8b87b14d82c6bce71cacda0e
Krzysztof Baranski <krisbaranski@gmail.com>
2021-11-03 20:50:51 +0100

    fix: Bugs & vunerabilities in mode modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,10 @@ const Header = props => {
       <section className="header">
         <h1 className="hero_title">Frontend Developer Portfolio</h1>
         <h4 className="hero_message">
-          passionate about creating new projects
-          <br /> changing the way, you experience the web
+          passionate about creating projects
+          <br />
+          that challenge and give
+          <br /> changing the way you experience the web
         </h4>
       </section>
     </header>
