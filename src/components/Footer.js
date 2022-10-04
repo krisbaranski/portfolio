c10cb90191d782e283b76596a12fa6f9074fb2b8
Krzysztof Baranski <krisbaranski@gmail.com>
2022-10-04 21:34:32 +0200

    update css, footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,15 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="container footer">
+      <section className="footer_social">
+        <p>footer</p>
+        <p>footer</p>
+        <p>footer</p>
+      </section>
       <section className="footer_text">
         <p>footer</p>
-        <p>footer</p>
-        <p>footer</p>
-        <p>footer</p>
+        <p>Privacy & Imprint</p>
+        <p>© 2022 Berlin</p>
       </section>
     </div>
   );
