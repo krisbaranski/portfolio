ae44f1aeef829d239a46897b77eec7ba27b421ae
Krzysztof Baranski <krisbaranski@gmail.com>
2022-10-15 13:46:14 +0200

    feat: spelling correction

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,7 +1,7 @@
 const headerData = [
   {
     title: 'Frontend Developer Portfolio',
-    text: 'passionate about creating new challenging projects, which will change the way, you experiance the web',
+    text: 'passionate about creating projects, which will change the way, you experience the web',
   },
 ];
 export default headerData;
