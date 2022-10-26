e617d02e24cfa198f2e235aa896c1f468261fd49
Krzysztof Baranski <krisbaranski@gmail.com>
2022-10-26 19:56:48 +0200

    create pics folder, additional components

---
+++ src/components/Background.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import Particles from 'react-tsparticles';
+
+const Background = () => {
+  const particlesInit = main => {
+    console.log(main);
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = container => {
+    console.log(container);
+  };
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: 'transparent',
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            onHover: {
+              enable: true,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 200,
+              duration: 1,
+              opacity: 0.3,
+              size: 3,
+            },
+            push: {
+              quantity: 1,
+            },
+            repulse: {
+              distance: 130,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#504611',
+          },
+          links: {
+            color: '#a7830c',
+            distance: 450,
+            enable: true,
+            opacity: 0.2,
+            width: 1,
+          },
+          collisions: {
+            enable: false,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'bounce',
+            random: false,
+            speed: 1.1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 700,
+            },
+            value: 50,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            random: true,
+            value: 2,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+export default Background;
---
+++ src/components/Contact.js
@@ -0,0 +1,149 @@
+import React from 'react';
+
+const Contact = props => {
+  return (
+    <div className="container">
+      <form className="container" onSubmit={props.handleSubmit} method="POST">
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            aria-describedby="emailHelp"
+            placeholder="your email adress"
+            autocomplete="off"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Subject</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="your subject"
+            autocomplete="off"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Message</label>
+          <textarea
+            className="form-control"
+            rows="10"
+            placeholder="your message"
+            autocomplete="off"
+            required
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+      */}
+    </div>
+  );
+};
+
+// export default ContactForm;
+// import React from 'react';
+
+// class ContactForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email: '',
+//       name: '',
+//       message: '',
+//     };
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+
+//     fetch('http://localhost:3002/send', {
+//       method: 'POST',
+//       body: JSON.stringify(this.state),
+//       headers: {
+//         Accept: 'application/json',
+//         'Content-Type': 'application/json',
+//       },
+//     })
+//       .then(response => response.json())
+//       .then(response => {
+//         if (response.status === 'success') {
+//           alert('Message Sent.');
+//           this.resetForm();
+//         } else if (response.status === 'fail') {
+//           alert('Message failed to send.');
+//         }
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <form
+//           id="contact-form"
+//           onSubmit={this.handleSubmit.bind(this)}
+//           method="POST"
+//         >
+//           <div className="form-group">
+//             <label htmlFor="exampleInputEmail1">Email</label>
+//             <input
+//               type="email"
+//               className="form-control"
+//               aria-describedby="emailHelp"
+//               value={this.state.email}
+//               placeholder="your email address"
+//             autocomplete = 'off';
+//               onChange={this.onEmailChange.bind(this)}
+//             />
+//           </div>
+//           <div className="form-group">
+//             <label htmlFor="subject">Subject</label>
+//             <input
+//               type="text"
+//               className="form-control"
+//               value={this.state.subject}
+//               placeholder="your subject"
+//             autocomplete = 'off';
+//               onChange={this.onSubjectChange.bind(this)}
+//             />
+//           </div>
+
+//           <div className="form-group">
+//             <label htmlFor="message">Message</label>
+//             <textarea
+//               className="form-control"
+//               rows="5"
+//               value={this.state.message}
+//               placeholder="your message"
+//             autocomplete = 'off';
+//               onChange={this.onMessageChange.bind(this)}
+//             />
+//           </div>
+//           <button type="submit" className="btn btn-primary">
+//             Submit
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+
+//   onEmailChange(event) {
+//     this.setState({ email: event.target.value });
+//   }
+
+//   onSubjectChange(event) {
+//     this.setState({ subject: event.target.value });
+//   }
+
+//   onMessageChange(event) {
+//     this.setState({ message: event.target.value });
+//   }
+// }
+// }
+
+export default Contact;
