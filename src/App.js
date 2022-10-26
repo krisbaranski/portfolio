b85aa7b6341e9c8cd38669881395e638cf15c2e9
Krzysztof Baranski <krisbaranski@gmail.com>
2022-10-26 19:57:42 +0200

    feat: modified files, add new favicon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Background from './components/Background';
 import Menu from './components/Menu';
+import { Sideboard } from './components/Sideboard';
 import Header from './components/Header';
 import { Aboutme } from './components/Aboutme';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div className="container">
+        <Background />
         <Menu />
+        <Sideboard />
         <Header />
         <Aboutme />
         <Projects />
+        <Contact />
         <Footer />
       </div>
     </Router>
