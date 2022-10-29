f1d93a4a6bba9ee6ff13a5430492d4f34552240c
Krzysztof Baranski <krisbaranski@gmail.com>
2022-10-29 11:27:33 +0200

    style: add padding

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Aboutme = () => {
   return (
     <div name="about" className="w-full  text-white ">
-      <div className="flex flex-col justify-center items-center w-full py-14 bg-[#264e86]">
+      <div className="flex flex-col justify-center items-center w-full py-20 bg-[#264e86]">
         <div className="max-w-[800px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-2 border-white">
