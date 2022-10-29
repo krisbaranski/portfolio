402786c39dc04aed30b807c02e13af1fb0514843
Krzysztof Baranski <krisbaranski@gmail.com>
2022-10-29 11:14:37 +0200

    style: add bg color, change font weight, color

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export const Aboutme = () => {
   return (
-    <div name="about" className="w-full h-screen text-[#264e86]">
-      <div className="flex flex-col justify-center items-center w-full h-full">
+    <div name="about" className="w-full h-screen text-white ">
+      <div className="flex flex-col justify-center items-center w-full h-screen bg-[#264e86]">
         <div className="max-w-[800px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-2 border-[#264e86]">
+            <p className="text-4xl font-bold inline border-b-2 border-white">
               About me
             </p>
           </div>
