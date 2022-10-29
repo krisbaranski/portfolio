38b0ea473a8afb0ff0a8986b0be81e37d60c9abc
Krzysztof Baranski <krisbaranski@gmail.com>
2022-10-29 11:32:23 +0200

    style: add padding

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -5,14 +5,14 @@ export const Aboutme = () => {
     <div name="about" className="w-full  text-white ">
       <div className="flex flex-col justify-center items-center w-full py-20 bg-[#264e86]">
         <div className="max-w-[800px] w-full grid grid-cols-2 gap-8">
-          <div className="sm:text-right pb-8 pl-4">
+          <div className="sm:text-right py-8 pl-4">
             <p className="text-4xl font-bold inline border-b-2 border-white">
               About me
             </p>
           </div>
           <div></div>
         </div>
-        <div className="max-w-[600px] md:max-w-[800px] w-full grid sm:grid-cols-2 gap-8 px-6">
+        <div className="max-w-[600px] md:max-w-[800px] w-full grid sm:grid-cols-2 gap-8 px-6 pb-6">
           <div className="sm:text-right text-2xl md:text-4xl font-bold">
             <p>Hi. I'm Kris, nice to meet you. Let me introduce myself.</p>
           </div>
