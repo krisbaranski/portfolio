01db233b6b269c9f59158736008932241ff312e7
Krzysztof Baranski <krisbaranski@gmail.com>
2022-11-01 20:28:44 +0100

    refactor: new zIndex

--- src/components/Sideboard.js
+++ src/components/Sideboard.js
@@ -6,7 +6,7 @@ import { BsFillPersonLinesFill } from 'react-icons/bs';
 const Sideboard = () => {
   return (
     <div>
-      <div className="hidden sm:flex fixed flex-col top-[35%] left-0">
+      <div className="hidden sm:flex fixed flex-col top-[35%] left-0 z-50">
         <ul>
           <li className="w-[160px] h-[40px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-sky-600">
             <a
