866b3d16d9b89a48dc9be907a8edabe7a7fec9e6
Krzysztof Baranski <krisbaranski@gmail.com>
2022-11-01 20:29:43 +0100

    refactor: update text, change bg color and opacity

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -3,13 +3,13 @@ import React from 'react';
 export const Aboutme = () => {
   return (
     <div name="about" className="w-full  text-white ">
-      <div className="flex flex-col justify-center items-center w-full py-20 bg-[#264e86]">
+      <div className="flex flex-col justify-center items-center w-full py-20 bg-[#0467ce] opacity-60">
         <div className="py-8">
           <p className="text-4xl font-bold text-left inline border-b-2 border-white">
             About me
           </p>
         </div>
-        <div className="max-w-[400px] sm:max-w-[500px] md:max-w-[720px] lg:max-w-[950px] w-full grid sm: grid-cols-2 md:grid-cols-3 gap-8 px-6 pb-8 font-semibold tracking-wide">
+        <div className="max-w-[400px] sm:max-w-[500px] md:max-w-[720px] lg:max-w-[950px] w-full grid md:grid-cols-3 gap-8 px-6 pb-8 font-semibold tracking-wide">
           <div>
             <p>Hi. I'm Kris, nice to meet you. Let me introduce myself.</p>
             <p>
@@ -27,11 +27,7 @@ export const Aboutme = () => {
             </p>
           </div>
           <div>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
-              maxime excepturi quidem aut ullam eius pariatur ad repudiandae
-              recusandae doloribus.
-            </p>
+            <p>More is coming soon</p>
           </div>
         </div>
       </div>
