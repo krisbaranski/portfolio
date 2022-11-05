7acff239d16705e5acbbb7ca332fbe8ee7a95477
Krzysztof Baranski <krisbaranski@gmail.com>
2022-11-05 18:53:51 +0100

    refactor: update About text

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -12,22 +12,31 @@ export const Aboutme = () => {
         <div className="max-w-[400px] sm:max-w-[500px] md:max-w-[720px] lg:max-w-[950px] w-full grid md:grid-cols-3 gap-8 px-6 pb-8 font-semibold tracking-wide">
           <div>
             <p>Hi. I'm Kris, nice to meet you. Let me introduce myself.</p>
+            <br />
             <p>
-              I am passionate about building software that improves the lives of
-              those around me. I specialize in creating software for clients
-              ranging from individuals to small-businesses.
+              I'm originally coming from Poland, where i studied fine arts,
+              worked as designer and teacher. Then 2005 i moved to Germany and
+              started my professional artist career. Mostly i'm working with
+              classical printing, painting and sculpture. So, how did i get into
+              programming?
             </p>
           </div>
           <div>
             <p>
-              My journey began with fine art, went through handicrafts into
-              programming. It was always my greatest pleasure and satisfaction
-              to create something exeptional. Programming gives me unlimited
-              potential to do so.
+              My journey began with fine arts. It went through handicrafts like
+              working with wood into programming. It was always my greatest
+              pleasure and satisfaction to create something exeptional. Thats
+              why i'm trying myself in different kinds if creative and technical
+              fields to find out the best solutions and beauty. Programming
+              gives me unlimited potential to be creative.
             </p>
           </div>
           <div>
-            <p>More is coming soon</p>
+            <p>
+              I am passionate about building software and web development. I
+              specialize in creating software for clients ranging from
+              individuals to small-businesses.
+            </p>
           </div>
         </div>
       </div>
