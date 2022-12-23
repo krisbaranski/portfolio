8171edca9c375df299e684493b1bb42685112ede
Krzysztof Baranski <krisbaranski@gmail.com>
2022-12-23 13:46:38 +0100

    style: correct text, spelling, padding

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -7,8 +7,8 @@ export const Aboutme = () => {
   return (
     <div name="about" className="w-full  text-white py-12  z-50">
       <div className="flex flex-col justify-center items-center w-full py-20 bg-[#1A6CB8] opacity-80">
-        <div className="py-8 pb-4">
-          <p className="text-4xl font-bold text-left inline border-b-2 border-white">
+        <div className="py-8">
+          <p className="text-4xl font-bold text-left inline border-b-2 border-white  pb-8">
             About me
           </p>
         </div>
@@ -23,11 +23,11 @@ export const Aboutme = () => {
             <p>Hi. I'm Kris, nice to meet you. Let me introduce myself.</p>
             <br />
             <p>
-              I'm originally coming from Poland, where i studied fine arts,
-              worked as designer and teacher. Then 2005 i moved to Germany,
-              teached fine arts, worked for art galeries and started my own
-              professional artist career. Mostly i'm working with classical
-              printing, painting and sculpture.
+              Originally i come from Poland, where i studied fine arts, worked
+              as designer and teacher. Then 2005 i moved to Germany, teached
+              fine arts, worked for art galeries and started my own professional
+              artist career. Mostly i'm working with classical printing,
+              painting and sculpture.
             </p>
           </div>
           <div>
@@ -37,8 +37,8 @@ export const Aboutme = () => {
             </p>
             <br />
             <p>
-              Its quite simple. Programing means to me a tool, a method or kind
-              of language for creating things on internet for ,others. To be
+              Its quite simple. Programming means to me a tool, a method, a kind
+              of language for creating things on internet for others. To be
               visible, present values, informations etc. So to do it, one needs
               to learn how it works in new technologies. It is not far away from
               another media used in fine arts. There are even many artists
@@ -50,19 +50,19 @@ export const Aboutme = () => {
               first websites myself, to have full control of structure, design
               and to learn new skills. My preferenences are often to focus on
               technical aspects, to understand which posibilities the technology
-              has. It is always my great pleasure and satisfaction to create
-              something exeptional. Thats why i'm trying myself in different
-              kinds of creative and technical fields to find out the best
-              solutions and beauty.
+              has.
             </p>
             <br />
           </div>
           <div>
             <p>
-              Programming gives me unlimited potential to be creative. I am
-              passionate about web development and building software. Especially
-              with JS libraries like ReactJS and frameworks like NextJS and
-              Angular.
+              It is always my great pleasure and satisfaction to create
+              something exeptional. Thats why i'm trying myself in different
+              kinds of creative and technical fields to find out the best
+              solutions and beauty. Programming gives me unlimited potential to
+              be creative. I am passionate about web development and building
+              software. Especially with JS libraries like ReactJS and frameworks
+              like NextJS and Angular.
             </p>
             <p>
               They are made for different purposes. So, depending on project,
