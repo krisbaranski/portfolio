c18c2ba694563836a18a1cbb2c3a2489077a6f74
Krzysztof Baranski <krisbaranski@gmail.com>
2024-01-30 12:56:43 +0100

    refactor: put style classes as const variables. adjust styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,13 +16,15 @@ const Navbar = () => {
 
   window.addEventListener('scroll', changeColor);
 
+  const MENU_LINK = 'p-2 rounded hover:bg-[#e7e5e4] hover:text-stone-800';
+
   return (
     <div>
       <div
         onScroll={changeColor}
         className={
           color
-            ? 'fixed w-full h-[60px] flex justify-between items-center md:px-14 z-20 bg-white shadow-md shadow-grey-300'
+            ? 'fixed w-full h-[60px] flex justify-between items-center md:px-14 z-20 backdrop-blur shadow-md shadow-grey-300'
             : 'fixed w-full h-[60px] flex justify-between items-center md:px-14 z-20 bg-transparent'
         }
       >
@@ -31,24 +33,14 @@ const Navbar = () => {
         </div>
 
         {/* Desktop menu */}
-        <ul className="hidden md:flex text-[#264e86] font-medium">
+        <ul className="hidden md:flex text-[#e7e5e4] font-medium">
           <li>
-            <Link
-              to="home"
-              smooth={true}
-              duration={500}
-              className="p-2 rounded hover:bg-[#264e86] hover:text-white"
-            >
+            <Link to="home" smooth={true} duration={500} className={MENU_LINK}>
               Home
             </Link>
           </li>
           <li>
-            <Link
-              to="about"
-              smooth={true}
-              duration={500}
-              className="p-2 rounded hover:bg-[#264e86] hover:text-white"
-            >
+            <Link to="about" smooth={true} duration={500} className={MENU_LINK}>
               About
             </Link>
           </li>
@@ -57,7 +49,7 @@ const Navbar = () => {
               to="skills"
               smooth={true}
               duration={500}
-              className="p-2 rounded hover:bg-[#264e86] hover:text-white"
+              className={MENU_LINK}
             >
               Skills
             </Link>
@@ -67,7 +59,7 @@ const Navbar = () => {
               to="projects"
               smooth={true}
               duration={500}
-              className="p-2 rounded hover:bg-[#264e86] hover:text-white"
+              className={MENU_LINK}
             >
               Projects
             </Link>
@@ -77,7 +69,7 @@ const Navbar = () => {
               to="contact"
               smooth={true}
               duration={500}
-              className="p-2 rounded hover:bg-[#264e86] hover:text-white"
+              className={MENU_LINK}
             >
               Contact
             </Link>
@@ -88,7 +80,7 @@ const Navbar = () => {
       {/* Hamburger */}
       <div onClick={handleClick} className="md:hidden">
         {!nav ? (
-          <FaBars className="fixed top-6 right-10 flex flex-col justify-center items-center text-[#264e86] z-30" />
+          <FaBars className="fixed top-6 right-10 flex flex-col justify-center items-center text-[#e7e5e4] z-30" />
         ) : (
           <FaTimes className="fixed top-6 right-10 flex flex-col justify-center items-center text-white z-30" />
         )}
@@ -99,7 +91,7 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'fixed top-0 left-0 w-full h-screen bg-[#264e86] flex flex-col justify-center items-center text-white z-20'
+            : 'fixed top-0 left-0 w-full h-screen bg-[#e7e5e4] flex flex-col justify-center items-center text-white z-20'
         }
       >
         <li className="py-6 text-4xl">
