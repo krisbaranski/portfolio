ace33434cdd0e34076b4b216ae04ce296c0c2930
Krzysztof Baranski <krisbaranski@gmail.com>
2024-01-30 13:38:31 +0100

    style/docs: adjust new colors, text

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,10 @@ const Header = () => {
           Welcome <br />
           to my
         </h1>
-        <h2 className="text-5xl sm:text-6xl md:text-7xl font-bold text-center text-[#e7e5e4] uppercase my-6 z-10">
+        <h2 className="flex justify-center text-5xl sm:text-6xl md:text-7xl font-bold text-center text-[#e7e5e4] uppercase my-6 z-10">
           portfolio
         </h2>
-        <p className="display-flex justify-center text-[#e7e5e4] py-4 max-w-[700px] font-light text-center z-10">
+        <p className="flex justify-center text-[#e7e5e4] py-4 max-w-[700px] font-light text-center z-10">
           My name is Kris and i specialize in frontend web development with
           focus on building responsive web applications and designing
           exceptional digital experiences.
