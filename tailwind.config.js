edfb92c5c7d4901e9047f337bbd08058db8d8594
Krzysztof Baranski <krisbaranski@gmail.com>
2024-01-31 21:23:18 +0100

    style: add new font for header design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        elephant: ['Elephant', 'sans'],
+      },
+      fontSize: {
+        '10xl': '10rem',
+        '11xl': '12.5rem',
+        '12xl': '15rem',
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        lighter: '300',
+      },
+      screens: {
+        xsm: '420px',
+      },
+    },
   },
   plugins: [],
 };
