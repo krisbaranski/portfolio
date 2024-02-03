730f6d3ef6acd261b2407181c873c55d15cf89d8
Krzysztof Baranski <krisbaranski@gmail.com>
2024-02-03 23:23:47 +0100

    style/docs/refactor: new icons on flip cards, new text, adjust buttons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,19 +6,33 @@ import DiceRoll from '../assets/DiceRoll.png';
 import Implement from '../assets/Implement.png';
 import GuessNr from '../assets/GuessNr.png';
 
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import TypeScript from '../assets/typescript.png';
+import Angular from '../assets/angular.png';
+import GatsbyImg from '../assets/gatsby.png';
+import ReactImg from '../assets/react.png';
+import NextImg from '../assets/nextjs.png';
+import Node from '../assets/node.png';
+import GitHub from '../assets/github.png';
+import MaterialUI from '../assets/materialui.png';
+import Tailwind from '../assets/tailwind.png';
+import Bash from '../assets/bash.png';
+
 const CARDS = 'grid md:grid-cols-2 lg:grid-cols-3 gap-4 z-10';
 const CARD = 'group h-64 [perspective:3200px]';
 const FLIP_CARD =
   'transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]';
 
 const CARD_FRONT =
-  'absolute inset-0 group-hover:opacity-0 duration-300 shadow-md shadow-black/40 group container rounded flex justify-center items-center mx-auto content-div';
+  'absolute inset-0 content-div group group-hover:opacity-0 duration-300 shadow-md shadow-black/40 container rounded flex justify-center items-center mx-auto';
 
 const CARD_BACK =
-  'absolute inset-0 content-div bg-slate-800 [transform:rotateY(180deg)] [backface-visibility:hidden]';
+  'absolute inset-0 content-div bg-slate-800 rounded [transform:rotateY(180deg)] [backface-visibility:hidden]';
 
 const CARD_TEXT =
-  'flex justify-center items-center text-center text-xl font-light text-white tracking-wider';
+  'flex justify-center items-center text-center text-lg font-light text-white tracking-wider';
 
 const BUTTON_CLASS =
   'text-center rounded px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:bg-slate-900 hover:text-sky-200';
@@ -51,10 +65,20 @@ const Projects = () => {
               ></div>
               {/* Hover Effects */}
               <div className={CARD_BACK}>
-                <span className={CARD_TEXT}>
-                  Professional Portfolio, ReactJS, CSS3, JSON, framer-motion
-                  animation
-                </span>
+                <span className={CARD_TEXT}>Profesional Artist Portfolio</span>
+                <div className="flex justify-center flex-row pt-4">
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={ReactImg}
+                    alt="React icon"
+                  />
+                  <img className="w-10 mx-auto p-1" src={CSS} alt="CSS icon" />
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={JavaScript}
+                    alt="JS icon"
+                  />
+                </div>
                 <div className="pt-8 text-center">
                   <a
                     target="_blank"
@@ -63,13 +87,13 @@ const Projects = () => {
                   >
                     <button className={BUTTON_CLASS}>Web Page</button>
                   </a>
-                  <a
+                  {/* <a
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://github.com/krisbaranski/mahakala-art"
                   >
                     <button className={BUTTON_CLASS}>Code</button>
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
@@ -84,9 +108,19 @@ const Projects = () => {
               ></div>
               {/* Hover Effects */}
               <div className={CARD_BACK}>
-                <span className={CARD_TEXT}>
-                  ReactJS App, user fetch API, Netlify
-                </span>
+                <span className={CARD_TEXT}>User Fetch Github App</span>
+                <div className="flex justify-center flex-row pt-4">
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={ReactImg}
+                    alt="React icon"
+                  />
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={MaterialUI}
+                    alt="MaterialUI icon"
+                  />
+                </div>
                 <div className="pt-8 text-center">
                   <a
                     target="_blank"
@@ -116,9 +150,21 @@ const Projects = () => {
               ></div>
               <div className={CARD_BACK}>
                 <span className={CARD_TEXT}>
-                  Dashboard React App, MUI, Yup Validation, Nivo Charts,
-                  Calendar
+                  Dashboard App <br /> Contacts, Validation, Charts, Calendar
+                  aso.
                 </span>
+                <div className="flex justify-center flex-row pt-4">
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={ReactImg}
+                    alt="React icon"
+                  />
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={MaterialUI}
+                    alt="MaterialUI icon"
+                  />
+                </div>
                 <div className="pt-8 text-center">
                   <a
                     target="_blank"
@@ -149,8 +195,21 @@ const Projects = () => {
               {/* Hover Effects */}
               <div className={CARD_BACK}>
                 <span className={CARD_TEXT}>
-                  Dice roll game made in JS, CSS3, HTML5
+                  "PIG GAME" <br /> a dice roll game for two players
                 </span>
+                <div className="flex justify-center flex-row pt-4">
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={HTML}
+                    alt="HTML icon"
+                  />
+                  <img className="w-10 mx-auto p-1" src={CSS} alt="CSS icon" />
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={JavaScript}
+                    alt="JS icon"
+                  />
+                </div>
                 <div className="pt-8 text-center">
                   <a
                     target="_blank"
@@ -159,13 +218,13 @@ const Projects = () => {
                   >
                     <button className={BUTTON_CLASS}>Demo</button>
                   </a>
-                  <a
+                  {/* <a
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://github.com/krisbaranski/diceroll/tree/master"
                   >
                     <button className={BUTTON_CLASS}>Code</button>
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
@@ -181,9 +240,22 @@ const Projects = () => {
               {/* Hover Effects */}
               <div className={CARD_BACK}>
                 <span className={CARD_TEXT}>
-                  JavaScript, CSS, HTML implementations
+                  50 implementations <br /> of web functionalities
                 </span>
-                <div className="pt-8 text-center">
+                <div className="flex justify-center flex-row pt-4">
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={HTML}
+                    alt="HTML icon"
+                  />
+                  <img className="w-10 mx-auto p-1" src={CSS} alt="CSS icon" />
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={JavaScript}
+                    alt="JS icon"
+                  />
+                </div>
+                <div className="pt-6 text-center">
                   <a
                     target="_blank"
                     rel="noopener noreferrer"
@@ -191,13 +263,13 @@ const Projects = () => {
                   >
                     <button className={BUTTON_CLASS}>Demo</button>
                   </a>
-                  <a
+                  {/* <a
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://github.com/krisbaranski/implementations"
                   >
                     <button className={BUTTON_CLASS}>Code</button>
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
@@ -212,7 +284,22 @@ const Projects = () => {
               ></div>
               {/* Hover Effects */}
               <div className={CARD_BACK}>
-                <span className={CARD_TEXT}>JavaScript Vintage Game</span>
+                <span className={CARD_TEXT}>
+                  Vintage Game <br /> to guess a number and score
+                </span>
+                <div className="flex justify-center flex-row pt-4">
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={HTML}
+                    alt="HTML icon"
+                  />
+                  <img className="w-10 mx-auto p-1" src={CSS} alt="CSS icon" />
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={JavaScript}
+                    alt="JS icon"
+                  />
+                </div>
                 <div className="pt-8 text-center">
                   <a
                     target="_blank"
@@ -221,13 +308,13 @@ const Projects = () => {
                   >
                     <button className={BUTTON_CLASS}>Demo</button>
                   </a>
-                  <a
+                  {/* <a
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://github.com/krisbaranski/guess_number_game"
                   >
                     <button className={BUTTON_CLASS}>Code</button>
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
