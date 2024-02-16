c9332eae26342048c825e1f1f28f24b7fda15fea
Krzysztof Baranski <krisbaranski@gmail.com>
2024-02-16 21:24:05 +0100

    refactor: change structure from single page to multiple linked pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import ScrollToTop from './components/ScrollToTop';
 import Background from './components/Background';
 import Navbar from './components/Navbar';
 import Sideboard from './components/Sideboard';
-import Header from './components/Header';
-import { Aboutme } from './components/Aboutme';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import AnimatedRoutes from './components/AnimatedRoutes';
 
 function App() {
   return (
     <Router>
-      <Background />
-      <Navbar />
-      <Sideboard />
-      <Header />
-      <Aboutme />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+      <ScrollToTop>
+        <Background />
+        <Navbar />
+        <Sideboard />
+        <AnimatedRoutes />
+      </ScrollToTop>
     </Router>
   );
 }
