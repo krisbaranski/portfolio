5bec0f4fe979c1c65720d410bf6c462a4edc807d
Krzysztof Baranski <krisbaranski@gmail.com>
2024-02-24 22:56:24 +0100

    style: restructure skills, add new containers and grid properties

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,6 @@ import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import TypeScript from '../assets/typescript.png';
 import Angular from '../assets/angular.png';
-import GatsbyImg from '../assets/gatsby.png';
 import ReactImg from '../assets/react.png';
 import NextImg from '../assets/nextjs.png';
 import Node from '../assets/node.png';
@@ -16,7 +15,9 @@ import Tailwind from '../assets/tailwind.png';
 import Bash from '../assets/bash.png';
 
 const BUTTON_CLASS =
-  'p-2 rounded shadow-md shadow-[#040c16] hover:scale-110 duration-300 z-20';
+  'p-1 rounded shadow-md shadow-[#040c1660] hover:scale-105 duration-200 z-20';
+const BENTO_GRID =
+  'grid grid-rows-1 grid-flow-col gap-4 bg-[#cffafe20] rounded-xl p-4';
 
 const Skills = () => {
   return (
@@ -38,62 +39,81 @@ const Skills = () => {
           </p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 self-center text-center py-8 z-10">
-          <div className={BUTTON_CLASS + ' bg-rose-900 text-white'}>
-            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
-            <p className="my-4">HTML</p>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 self-center text-center p-8 z-10">
+          {/* Bento Grid*/}
+          <div className={BENTO_GRID + ' col-span-2'}>
+            <div className={BUTTON_CLASS + ' bg-rose-900 text-white'}>
+              <img className="h-16 mx-auto" src={HTML} alt="HTML icon" />
+              <p className="my-4">HTML</p>
+            </div>
+            <div className={BUTTON_CLASS + ' bg-blue-900 text-white'}>
+              <img className="h-16 mx-auto" src={CSS} alt="CSS icon" />
+              <p className="my-4">CSS</p>
+            </div>
           </div>
-          <div className={BUTTON_CLASS + ' bg-blue-900 text-white'}>
-            <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
-            <p className="my-4">CSS</p>
+          {/* Bento Grid*/}
+          <div className={BENTO_GRID + ' col-span-2'}>
+            <div className={BUTTON_CLASS + ' bg-amber-500 text-white'}>
+              <img className="h-16 mx-auto" src={JavaScript} alt="JS icon" />
+              <p className="my-4">JAVASCRIPT</p>
+            </div>
+            <div className={BUTTON_CLASS + ' bg-slate-100 text-blue-400'}>
+              <img className="h-16 mx-auto" src={TypeScript} alt="TS icon" />
+              <p className="my-4">TYPESCRIPT</p>
+            </div>
           </div>
-          <div className={BUTTON_CLASS + ' bg-amber-500 text-white'}>
-            <img className="w-20 mx-auto" src={JavaScript} alt="JS icon" />
-            <p className="my-4">JAVASCRIPT</p>
+          {/* Bento Grid*/}
+          <div className={BENTO_GRID}>
+            <div className={BUTTON_CLASS + ' bg-green-900 text-lime-400'}>
+              <img className="h-16 mx-auto" src={Node} alt="NODE icon" />
+              <p className="my-4">NODE JS</p>
+            </div>
           </div>
-          <div className={BUTTON_CLASS + ' bg-slate-100 text-blue-400'}>
-            <img className="w-20 mx-auto" src={TypeScript} alt="TS icon" />
-            <p className="my-4">TYPESCRIPT</p>
-          </div>
-          <div className={BUTTON_CLASS + ' bg-red-800 text-white'}>
-            <img className="w-20 mx-auto" src={Angular} alt="Angular icon" />
-            <p className="my-4">ANGULAR</p>
-          </div>
-          <div className={BUTTON_CLASS + ' bg-fuchsia-200 text-purple-700'}>
-            <img className="w-20 mx-auto" src={GatsbyImg} alt="Gatsby icon" />
-            <p className="my-4">GATSBY</p>
-          </div>
-          <div className={BUTTON_CLASS + ' bg-sky-800 text-white'}>
-            <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
-            <p className="my-4">REACT</p>
+          {/* Bento Grid*/}
+          <div className={BENTO_GRID + ' col-span-3'}>
+            <div className={BUTTON_CLASS + ' bg-red-800 text-white'}>
+              <img className="h-16 mx-auto" src={Angular} alt="Angular icon" />
+              <p className="my-4">ANGULAR</p>
+            </div>
+
+            <div className={BUTTON_CLASS + ' bg-sky-800 text-white'}>
+              <img className="h-16 mx-auto" src={ReactImg} alt="React icon" />
+              <p className="my-4">REACT</p>
+            </div>
+
+            <div className={BUTTON_CLASS + ' bg-slate-600 text-white'}>
+              <img className="h-16 mx-auto" src={NextImg} alt="NEXTJS icon" />
+              <p className="my-4">NEXTJS</p>
+            </div>
           </div>
 
-          <div className={BUTTON_CLASS + ' bg-slate-600 text-white'}>
-            <img className="w-20 mx-auto" src={NextImg} alt="NEXTJS icon" />
-            <p className="my-4">NEXTJS</p>
+          {/* Bento Grid*/}
+          <div className={BENTO_GRID + ' col-span-2'}>
+            <div className={BUTTON_CLASS + ' bg-sky-200 text-sky-400'}>
+              <img className="h-16 m-auto" src={MaterialUI} alt="MUI icon" />
+              <p className="my-4">MATERIALUI</p>
+            </div>
+            <div className={BUTTON_CLASS + ' bg-indigo-800 text-white'}>
+              <img
+                className="h-16 mx-auto"
+                src={Tailwind}
+                alt="Tailwind icon"
+              />
+              <p className="my-4">TAILWIND</p>
+            </div>
           </div>
-          <div className={BUTTON_CLASS + ' bg-green-900 text-lime-400'}>
-            <img className="w-20 mx-auto" src={Node} alt="NODE icon" />
-            <p className="my-4">NODE JS</p>
-          </div>
-          <div className={BUTTON_CLASS + ' bg-sky-200 text-sky-400'}>
-            <img className="w-20 m-auto" src={MaterialUI} alt="MUI icon" />
-            <p className="my-4">MATERIALUI</p>
-          </div>
-          <div className={BUTTON_CLASS + ' bg-indigo-800 text-white'}>
-            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon" />
-            <p className="my-4">TAILWIND</p>
-          </div>
-          <div className={BUTTON_CLASS + ' bg-lime-600 text-white'}>
-            <img className="w-20 mx-auto" src={Bash} alt="BASH icon" />
-            <p className="my-4">BASH</p>
-          </div>
-          <div className={BUTTON_CLASS + ' bg-slate-800 text-white'}>
-            <img className="w-20 mx-auto" src={GitHub} alt="GIT icon" />
-            <p className="my-4">GITHUB</p>
+          {/* Bento Grid*/}
+          <div className={BENTO_GRID + ' col-span-2'}>
+            <div className={BUTTON_CLASS + ' bg-lime-600 text-white'}>
+              <img className="h-16 mx-auto" src={Bash} alt="BASH icon" />
+              <p className="my-4">BASH</p>
+            </div>
+            <div className={BUTTON_CLASS + ' bg-slate-800 text-white'}>
+              <img className="h-16 mx-auto" src={GitHub} alt="GIT icon" />
+              <p className="my-4">GITHUB</p>
+            </div>
           </div>
         </div>
-        <div></div>
       </div>
     </motion.div>
   );
