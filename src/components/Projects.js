0e2b65a9e50a49ddb959c2a27f30108787c4d06e
Krzysztof Baranski <krisbaranski@gmail.com>
2024-03-22 19:22:02 +0100

    style: add new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import Mahakala from '../assets/Mahakala.png';
+import Binta from '../assets/BINTA.png';
+import EK from '../assets/EK.png';
 import Gfinder from '../assets/Gfinder.png';
 import Dash from '../assets/Dashboard.png';
 import DiceRoll from '../assets/DiceRoll.png';
@@ -10,15 +12,15 @@ import GuessNr from '../assets/GuessNr.png';
 import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
-// import TypeScript from '../assets/typescript.png';
-// import Angular from '../assets/angular.png';
+import TypeScript from '../assets/typescript.png';
+import Angular from '../assets/angular.png';
 // import GatsbyImg from '../assets/gatsby.png';
 import ReactImg from '../assets/react.png';
-// import NextImg from '../assets/nextjs.png';
+import NextImg from '../assets/nextjs.png';
 // import Node from '../assets/node.png';
 // import GitHub from '../assets/github.png';
 import MaterialUI from '../assets/materialui.png';
-// import Tailwind from '../assets/tailwind.png';
+import Tailwind from '../assets/tailwind.png';
 // import Bash from '../assets/bash.png';
 
 const CARDS = 'grid md:grid-cols-2 lg:grid-cols-3 gap-4 z-10';
@@ -91,13 +93,78 @@ const Projects = () => {
                   >
                     <button className={BUTTON_CLASS}>Web Page</button>
                   </a>
-                  {/* <a
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div className={CARD}>
+            <div className={FLIP_CARD}>
+              <div
+                style={{ backgroundImage: `url(${Binta})` }}
+                className={CARD_FRONT + ' bg-stone-900'}
+              ></div>
+              {/* Hover Effects */}
+              <div className={CARD_BACK}>
+                <span className={CARD_TEXT}>
+                  International Art Institute Webpage
+                </span>
+                <div className="flex justify-center flex-row pt-4">
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={NextImg}
+                    alt="React icon"
+                  />
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={Tailwind}
+                    alt="Tailwind icon"
+                  />
+                </div>
+                <div className="pt-8 text-center">
+                  <a
                     target="_blank"
                     rel="noopener noreferrer"
-                    href="https://github.com/krisbaranski/mahakala-art"
+                    href="https://diamondway-art.org"
                   >
-                    <button className={BUTTON_CLASS}>Code</button>
-                  </a> */}
+                    <button className={BUTTON_CLASS}>Web Page</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item */}
+          <div className={CARD}>
+            <div className={FLIP_CARD}>
+              <div
+                style={{ backgroundImage: `url(${EK})` }}
+                className={CARD_FRONT}
+              ></div>
+              {/* Hover Effects */}
+              <div className={CARD_BACK}>
+                <span className={CARD_TEXT}>Client Webpage</span>
+                <div className="flex justify-center flex-row pt-4">
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={Angular}
+                    alt="Angular icon"
+                  />
+                  <img className="w-10 mx-auto p-1" src={CSS} alt="CSS icon" />
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={TypeScript}
+                    alt="Typescript icon"
+                  />
+                </div>
+                <div className="pt-8 text-center">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://mahakala-art.com"
+                  >
+                    <button className={BUTTON_CLASS}>Web Page</button>
+                  </a>
                 </div>
               </div>
             </div>
@@ -193,58 +260,13 @@ const Projects = () => {
           <div className={CARD}>
             <div className={FLIP_CARD}>
               <div
-                style={{ backgroundImage: `url(${DiceRoll})` }}
-                className={CARD_FRONT}
-              ></div>
-              {/* Hover Effects */}
-              <div className={CARD_BACK}>
-                <span className={CARD_TEXT}>
-                  "PIG GAME" <br /> a dice roll game for two players
-                </span>
-                <div className="flex justify-center flex-row pt-4">
-                  <img
-                    className="w-10 mx-auto p-1"
-                    src={HTML}
-                    alt="HTML icon"
-                  />
-                  <img className="w-10 mx-auto p-1" src={CSS} alt="CSS icon" />
-                  <img
-                    className="w-10 mx-auto p-1"
-                    src={JavaScript}
-                    alt="JS icon"
-                  />
-                </div>
-                <div className="pt-8 text-center">
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://krisbaranski.github.io/diceroll/index.html"
-                  >
-                    <button className={BUTTON_CLASS}>Demo</button>
-                  </a>
-                  {/* <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/krisbaranski/diceroll/tree/master"
-                  >
-                    <button className={BUTTON_CLASS}>Code</button>
-                  </a> */}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* Grid Item */}
-          <div className={CARD}>
-            <div className={FLIP_CARD}>
-              <div
                 style={{ backgroundImage: `url(${Implement})` }}
                 className={CARD_FRONT}
               ></div>
               {/* Hover Effects */}
               <div className={CARD_BACK}>
                 <span className={CARD_TEXT}>
-                  50 implementations <br /> of web functionalities
+                  Implementations <br /> of web functionality
                 </span>
                 <div className="flex justify-center flex-row pt-4">
                   <img
@@ -267,13 +289,44 @@ const Projects = () => {
                   >
                     <button className={BUTTON_CLASS}>Demo</button>
                   </a>
-                  {/* <a
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item */}
+          <div className={CARD}>
+            <div className={FLIP_CARD}>
+              <div
+                style={{ backgroundImage: `url(${DiceRoll})` }}
+                className={CARD_FRONT}
+              ></div>
+              {/* Hover Effects */}
+              <div className={CARD_BACK}>
+                <span className={CARD_TEXT}>
+                  Dice roll <br /> game for two players
+                </span>
+                <div className="flex justify-center flex-row pt-4">
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={HTML}
+                    alt="HTML icon"
+                  />
+                  <img className="w-10 mx-auto p-1" src={CSS} alt="CSS icon" />
+                  <img
+                    className="w-10 mx-auto p-1"
+                    src={JavaScript}
+                    alt="JS icon"
+                  />
+                </div>
+                <div className="pt-8 text-center">
+                  <a
                     target="_blank"
                     rel="noopener noreferrer"
-                    href="https://github.com/krisbaranski/implementations"
+                    href="https://krisbaranski.github.io/diceroll/index.html"
                   >
-                    <button className={BUTTON_CLASS}>Code</button>
-                  </a> */}
+                    <button className={BUTTON_CLASS}>Demo</button>
+                  </a>
                 </div>
               </div>
             </div>
@@ -289,7 +342,7 @@ const Projects = () => {
               {/* Hover Effects */}
               <div className={CARD_BACK}>
                 <span className={CARD_TEXT}>
-                  Vintage Game <br /> to guess a number and score
+                  Vintage Game <br /> guess a number
                 </span>
                 <div className="flex justify-center flex-row pt-4">
                   <img
@@ -312,13 +365,6 @@ const Projects = () => {
                   >
                     <button className={BUTTON_CLASS}>Demo</button>
                   </a>
-                  {/* <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/krisbaranski/guess_number_game"
-                  >
-                    <button className={BUTTON_CLASS}>Code</button>
-                  </a> */}
                 </div>
               </div>
             </div>
