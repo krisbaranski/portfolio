119d00fdba6afd16477849de71b837cf84fc8b27
Krzysztof Baranski <krisbaranski@gmail.com>
2024-05-04 21:10:49 +0200

    refactor: add metadata for better SEO

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import usePageSEO from './hooks/usePageSEO';
 
 import ScrollToTop from './components/ScrollToTop';
 import Background from './components/Background';
@@ -9,6 +10,28 @@ import AnimatedRoutes from './components/AnimatedRoutes';
 import Footer from './components/Footer';
 
 function App() {
+  usePageSEO({
+    title: 'Kris Baranski | Web developer',
+    description:
+      'I specialize in frontend web development with focus on building responsive web applications and designing exceptional digital experiences.',
+    keywords: [
+      'portfolio',
+      'frontend',
+      'web',
+      'development',
+      'react',
+      'UX/UI',
+      'framer-motion',
+      'Java Script',
+      'Tailwind',
+    ],
+    ogTitle: 'Kris Baranski | Web developer',
+    ogDescription:
+      'I specialize in frontend web development with focus on building responsive web applications and designing exceptional digital experiences.',
+    ogImage:
+      'https://www.krisbaranski.com/static/media/portrait.b27d02955d8b412c45d5.jpg',
+    ogUrl: 'https://krisbaranski.com/',
+  });
   return (
     <Router>
       <ScrollToTop>
